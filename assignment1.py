03245d4f605e6994aa8b387be4624e565a4a8390
Will Ballentine <Will.Ballentine@waystar.com>
2023-02-08 11:53:28 -0500

    inital commit

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+
+
+print("hello world!")
+
+my_list = ['thing1', 'thing2', 'thing3', 'thing4', 'thing5']
+
+print(my_list[0])
+
+
+for thing in my_list:
+    print(thing)
+
+my_dict = {'first key': 'first value', 'second key': 'second value'}
+print(my_dict['second key'])
+
+
+my_tuple = ("yes", "no", "maybe")
+
+print(my_tuple[0])
